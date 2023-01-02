793c8d3f67875a458986f50aeb420bfed1bd4075
Philip Crockett <contact@philcrockett.com>
2023-01-02 22:56:06 +0100

    Fix test script

--- script/test.ml
+++ script/test.ml
@@ -1,9 +1,8 @@
 #!/usr/bin/env ocaml
 
 let log_info = print_endline
-let log_verbose = prerr_endline
 
-type test_result = Ok of string | Fail of string
+type test_result = Pass of string | Fail of string
 
 let run_tests file_path : test_result =
   let root_dir = Filename.dirname file_path in
@@ -15,12 +14,12 @@ let run_tests file_path : test_result =
   | 0 -> (
       match Sys.file_exists (file_path ^ ".corrected") with
       | true -> Fail file_path
-      | false -> Ok file_path)
+      | false -> Pass file_path)
   | _ -> Fail file_path
 
 let handle_result result =
   (match result with
-  | Ok file_path -> Filename.basename file_path ^ ": OK"
+  | Pass file_path -> Filename.basename file_path ^ ": OK"
   | Fail file_path -> Filename.basename file_path ^ ": FAIL")
   |> log_info;
   result
@@ -28,15 +27,27 @@ let handle_result result =
 let all_test_files root_dir =
   Sys.readdir root_dir |> Array.to_seq
   |> Seq.filter (String.ends_with ~suffix:".ml.md")
+  |> Seq.map (fun file_name -> root_dir ^ Filename.dir_sep ^ file_name)
 
 let is_failure result = match result with Fail _ -> true | _ -> false
-let root_dir args = args.(0) |> Filename.dirname |> Filename.dirname
+
+let root_dir args =
+  match args with
+  | [| _ |] -> Ok "."
+  | [| _; dir_path |] -> Ok dir_path
+  | _ -> Error "Expecting 1 argument: test directory path"
+
 let to_list seq = Seq.fold_left (fun a b -> a @ [ b ]) [] seq
 
 let main args =
+  let test_dir =
+    match root_dir args with
+    | Ok path -> path
+    | Error msg -> raise (Invalid_argument msg)
+  in
   match
-    args |> root_dir |> all_test_files |> Seq.map run_tests
-    |> Seq.map handle_result |> Seq.filter is_failure |> to_list
+    all_test_files test_dir |> Seq.map run_tests |> Seq.map handle_result
+    |> Seq.filter is_failure |> to_list
   with
   | [] -> 0
   | _ -> 1
